d1f0d99e78942d81e15bf52c12cc17dfc48e96a8
Vikas Singh <69408149+vikassingh135@users.noreply.github.com>
2023-05-31 17:05:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const returnPromis = () => {
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve();
+		},1000);
+	})
+}
+
+const successCallback = () => {
+	const output = document.getElementById("output");
+	output.innerText = "Hello, world!";
+}
+
+returnPromis().then(successCallback);
